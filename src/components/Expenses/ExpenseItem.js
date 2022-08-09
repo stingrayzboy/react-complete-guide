9f1a8b256ad3c0ce8410a29d600a40c325f5d31f
Faraz Noor <faraznoor75@gmail.com>
2022-08-09 23:35:39 +0530

    using set state to alter dom events.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.title);
+
+  console.log("Element Evaluated.")
+
   const clickHandler = () => {
-    console.log("clicked!!")
+    setTitle("Boom")
+    console.log(title)
   }
 
 
-  const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
