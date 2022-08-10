fafd6235c20072c99905bc10f825e954735fc33f
Faraz Noor <faraznoor75@gmail.com>
2022-08-10 21:46:25 +0530

    Using Key Prop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { useState } from "react";
 import "./App.css";
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
-
+const DUMMY_DATA = [
+  { id:"e1", title: "Car Insurance", date: new Date(2022, 2, 23), amount: "2332" },
+  { id:"e2", title: "Car Battery", date: new Date(2022, 3, 22), amount: "232" },
+  { id:"e3", title: "Bhosadpappu", date: new Date(2012, 2, 23), amount: "34" },
+  { id:"e4", title: "RTX 3060ti", date: new Date(2022, 1, 10), amount: "23423" },
+];
 const App = () => {
-  const expenses = [
-    { title: "Car Insurance", date: new Date(2022, 2, 23), amount: "2332" },
-    { title: "Car Battery", date: new Date(2022, 3, 22), amount: "232" },
-    { title: "Bhosadpappu", date: new Date(2012, 2, 23), amount: "34" },
-    { title: "RTX 3060ti", date: new Date(2022, 1, 10), amount: "23423" },
-  ];
+  const [expenses, setStateExpenses] = useState(DUMMY_DATA);
 
-  const onAddExpense = (data) =>{
-    console.log("From App.js")
-    console.table(data)
-  }
+  const onAddExpense = (data) => {
+    setStateExpenses((prevExpenses)=>{ return [data, ...prevExpenses]});
+  };
   return (
     <div>
-      <NewExpense onSavedExpense={onAddExpense}/>
+      <NewExpense onSavedExpense={onAddExpense} />
       <ExpenseList expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -4,7 +4,6 @@ import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 const ExpenseList = (props) => {
-  const expenses = props.expenses;
   const [selectedDate, setSelectedDate] = useState("2020");
   const onSelectedExpenseDate = (dateSelected) => {
     setSelectedDate(dateSelected);
@@ -18,6 +17,7 @@ const ExpenseList = (props) => {
       {props.expenses.map((x) => {
         return (
           <ExpenseItem
+            key={x.id}
             title={x.title}
             amount={x.amount}
             date={x.date}
