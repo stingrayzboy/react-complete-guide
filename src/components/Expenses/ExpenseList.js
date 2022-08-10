17338f302c4fff5484919daf312def462cb8e564
Faraz Noor <faraznoor75@gmail.com>
2022-08-10 21:23:25 +0530

    formatting

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -5,9 +5,9 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 const ExpenseList = (props) => {
   const expenses = props.expenses;
-  const [selectedDate, setSelectedDate] = useState('2020')
+  const [selectedDate, setSelectedDate] = useState("2020");
   const onSelectedExpenseDate = (dateSelected) => {
-    setSelectedDate(dateSelected)
+    setSelectedDate(dateSelected);
   };
   return (
     <Card className="expenses">
@@ -15,12 +15,14 @@ const ExpenseList = (props) => {
         baseDate={selectedDate}
         onSelectedExpenseDate={onSelectedExpenseDate}
       ></ExpensesFilter>
-      {props.expenses.map(x => { return(
-        <ExpenseItem
-        title={x.title}
-        amount={x.amount}
-        date={x.date}
-      ></ExpenseItem>)
+      {props.expenses.map((x) => {
+        return (
+          <ExpenseItem
+            title={x.title}
+            amount={x.amount}
+            date={x.date}
+          ></ExpenseItem>
+        );
       })}
     </Card>
   );
