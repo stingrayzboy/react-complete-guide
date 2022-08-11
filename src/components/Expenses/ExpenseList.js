89fc5fe79e90cf5e6fa511d71e2d4ddc97230fd1
Faraz Noor <faraznoor75@gmail.com>
2022-08-11 19:12:00 +0530

    Added Chart to show expenses

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import "./ExpenseList.css";
 import Expenses from "./Expenses";
+import ExpensesChart from "./ExpensesChart";
 const ExpenseList = (props) => {
   const [selectedDate, setSelectedDate] = useState("2020");
   const onSelectedExpenseDate = (dateSelected) => {
@@ -18,6 +19,7 @@ const ExpenseList = (props) => {
         baseDate={selectedDate}
         onSelectedExpenseDate={onSelectedExpenseDate}
       ></ExpensesFilter>
+      <ExpensesChart expenses={filteredExpenses}/>
       <Expenses items={filteredExpenses}></Expenses>
     </Card>
   );
